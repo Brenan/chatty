9893a842ae9974ca74f94d020f1bf54209b8e0e9
Brenan Klain <brenanklain@gmail.com>
2013-10-21 16:44:07 -0600

    Updates to chatty

--- server.js
+++ server.js
@@ -13,18 +13,12 @@ app.use(function(req,res,next){
 });
 
 app.get("/", function(req,res){
-	res.send(JSON.stringify(messages));
+	res.send(messages);
 });
 
 app.post("/", function(req,res){
    	var message =req.body;
-   	var today = new Date();
-	var dd = today.getDate();
-	var mm = today.getMonth()+1; //January is 0!
-
-	var yyyy = today.getFullYear();
-	if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = mm+'/'+dd+'/'+yyyy;
-   	message.createTime = today;
+   	message.createTime = new Date();
     messages.push(message);
 	res.send(messages);	   
 });
